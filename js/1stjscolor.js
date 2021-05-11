ad60077c13785e217009bcc014d96a3cbfb5f89e
정집인 <67492641+jaban0424@users.noreply.github.com>
2021-05-11 21:26:31 +0900

    Update 1stjscolor.js

--- js/1stjscolor.js
+++ js/1stjscolor.js
@@ -1,5 +1,7 @@
 var heading=document.querySelector('#heading');
-heading.onclick=function(){
+    var name=prompt("너 누군데?");
+    document.write("<b><big>"+name+"</b></big>, 어서와.");
+    heading.onclick=function(){
     heading.style.color="red";
     alert("누르지 말라고 했잖아?")
     confirm("'Devil.apk'(을)를 실행하시겠습니까?")
